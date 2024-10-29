c25646e6056fc85cd458f6c146507394f14bec92
XargonWan <XargonWan@gmail.com>
2024-10-29 11:21:16 +0900

    Submodules updated

--- update_from_upstream.sh
+++ update_from_upstream.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+git submodule init
+git submodule update
 git fetch https://github.com/flathub/org.zdoom.GZDoom master  # Fetch the latest changes from the remote master branch
 git merge FETCH_HEAD  # Merge the fetched changes into your current branch
