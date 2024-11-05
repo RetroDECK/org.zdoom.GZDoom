1794697d955ac14e5be42cdbd649efbf1fc0efb8
XargonWan <XargonWan@gmail.com>
2024-11-05 22:13:55 +0900

    Migrated to the new worklow

--- update_from_upstream.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git submodule init
-git submodule update
-git fetch https://github.com/flathub/org.zdoom.GZDoom master  # Fetch the latest changes from the remote master branch
-git merge FETCH_HEAD  # Merge the fetched changes into your current branch
